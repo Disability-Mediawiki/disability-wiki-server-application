b4e6733b4b8631edf4f43fca2975b64490f1e2b5
dhayananth <dhayanthdharma@gmail.com>
2021-06-09 17:13:03 +0200

    UPDATE UBUNTU FORMAT FILE PATH

--- application/main/config.py
+++ application/main/config.py
@@ -4,23 +4,64 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
+    # SECRET_KEY = os.getenv('SECRET_KEY', 'my_precious_secret_key')
+    # DEBUG = False
+    # MAIN_PATH = basedir
+    # UPLOAD_FOLDER = './resources/uploads'
+    # TEMP_DOC_FOLDER = basedir.rsplit('\\', 1)[0].rsplit('\\', 1)[
+    #     0]+'\\resources\\temp_doc'
+    # ORIGINAL_FILE_FOLDER = basedir.rsplit('\\', 1)[0].rsplit('\\', 1)[
+    #     0]+'\\resources\\uploads'
+    # RESULT_FOLDER = './resources/classified/result'
+    # UPLOAD_WIKIEDIT_REQUEST_FOLDER = './resources/classified/requested'
+    # UPLOAD_COMPLETED_FOLDER = './resources/classified/completed'
+
+    # # PYWIKICONFIGURATION
+    # PYWIKI_FAMILY_FILE = basedir.rsplit('\\', 1)[0].rsplit('\\', 1)[
+    #     0]+'\\config\\my_family.py'
+    # PYWIKI_USER_PASSWORD_FILE = basedir.rsplit('\\', 1)[0].rsplit('\\', 1)[
+    #     0]+'\\user-password.py'
+    # WIKI_USER_NAME = 'WikibaseAdmin'
+    # WIKI_BOT_USER_NAME = 'bot_user_1'
+    # WIKI_BOT_SECRET = 'cicb8eb65ktd956a0b43vauc4o63fpak'
+    # WIKI_SPARQL_END_POINT = 'http://localhost:8989/bigdata/namespace/wdq/sparql'
+    # WIKI_DOMAIN = 'localhost:8181'
+    # WIKI_PROTOCOL = 'http'
+    # WIKI_API_URI = 'http://localhost:8181/w/api.php'
+
+    # # ERROR LOG
+    # ERROR_LOG_FILE = './resources/error_log/debug_error.txt'
+
+    # # WIKIBASE PROPERTIES
+    # DOCUMENT_CLASS_QID = 'Q76'
+    # PARAGRAPH_CLASS_QID = 'Q79'
+    # TOPIC_CLASS_QID = 'Q'
+    # INSTACE_OF_PROPERTY_PID = 'P31'
+    # DOCUMENT_REFERENCE_URI_PROPERTY_PID = 'P32'
+    # PART_OF_DOCUMENT_PROPERTY_PID = 'P34'
+    # HAS_TEXT_PROPERTY_PID = 'P24'
+    # HAS_TOPIC_PROPERTY_PID = 'P4'
+    # HAS_PARAGRAPH_PROPERTY_PID = 'P33'
+    # MENTIONED_IN_PROPERTY_PID = 'P26'
+
+    # UBUNTU FORMAT************************************************************
     SECRET_KEY = os.getenv('SECRET_KEY', 'my_precious_secret_key')
     DEBUG = False
     MAIN_PATH = basedir
     UPLOAD_FOLDER = './resources/uploads'
-    TEMP_DOC_FOLDER = basedir.rsplit('\\', 1)[0].rsplit('\\', 1)[
-        0]+'\\resources\\temp_doc'
-    ORIGINAL_FILE_FOLDER = basedir.rsplit('\\', 1)[0].rsplit('\\', 1)[
-        0]+'\\resources\\uploads'
+    TEMP_DOC_FOLDER = basedir.rsplit('/', 1)[0].rsplit('/', 1)[
+        0]+'/resources/temp_doc'
+    ORIGINAL_FILE_FOLDER = basedir.rsplit('/', 1)[0].rsplit('/', 1)[
+        0]+'/resources/uploads'
     RESULT_FOLDER = './resources/classified/result'
     UPLOAD_WIKIEDIT_REQUEST_FOLDER = './resources/classified/requested'
     UPLOAD_COMPLETED_FOLDER = './resources/classified/completed'
 
     # PYWIKICONFIGURATION
-    PYWIKI_FAMILY_FILE = basedir.rsplit('\\', 1)[0].rsplit('\\', 1)[
-        0]+'\\config\\my_family.py'
-    PYWIKI_USER_PASSWORD_FILE = basedir.rsplit('\\', 1)[0].rsplit('\\', 1)[
-        0]+'\\user-password.py'
+    PYWIKI_FAMILY_FILE = basedir.rsplit('/', 1)[0].rsplit('/', 1)[
+        0]+'/config/my_family.py'
+    PYWIKI_USER_PASSWORD_FILE = basedir.rsplit('/', 1)[0].rsplit('/', 1)[
+        0]+'/user-password.py'
     WIKI_USER_NAME = 'WikibaseAdmin'
     WIKI_BOT_USER_NAME = 'bot_user_1'
     WIKI_BOT_SECRET = 'cicb8eb65ktd956a0b43vauc4o63fpak'
