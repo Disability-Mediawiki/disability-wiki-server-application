1c8cd92b94ddfb211e0e25b530adf54f43d3eecb
dhayananth <dhayanthdharma@gmail.com>
2021-12-14 11:13:40 +0100

    update diswiki wikibase entity id's

--- user-password.py
+++ user-password.py
@@ -1,2 +1,2 @@
 # (u'WikibaseAdmin', BotPassword(u'bot_user_1', u'o7ij9gcuvedc3134kvlp29ko3quoma0j'))
-(u'WikibaseAdmin', BotPassword(u'bot_user_1', u'l2184evfr3lvqfm69h5cg1orhaann8h0'))
+(u'WikibaseAdmin', BotPassword(u'bot_user_1', u'rgd9gj5jpaflqemnkh8k0vcl3qmde425'))
